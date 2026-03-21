b953fb10b6e1f2d2edf1061d6bb7964469aa4410
pottscale748-create <pottscale748@gmail.com>
2026-03-21 16:43:57 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request, abort
+import time
+import os
 
 app = Flask(__name__)
+
+# store tokens (temporary storage)
+valid_tokens = {}
+
+# create a test token (expires in 60 seconds)
+valid_tokens["test123"] = time.time() + 60
 
 @app.route("/")
 def home():
@@ -8,12 +16,20 @@ def home():
 
 @app.route("/download")
 def download():
+    token = request.args.get("token")
+
+    if token not in valid_tokens:
+        return abort(403)
+
+    # check expiration
+    if time.time() > valid_tokens[token]:
+        del valid_tokens[token]
+        return abort(403)
+
+    # one-time use (delete after use)
+    del valid_tokens[token]
+
     return send_file("pack.zip", as_attachment=True)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
-import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
