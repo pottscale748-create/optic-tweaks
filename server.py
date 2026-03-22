4753e69f4110311b7034e9097107d0b4a2bba7c5
pottscale748-create <pottscale748@gmail.com>
2026-03-21 17:02:58 -0700

    Update server.py

--- server.py
+++ server.py
@@ -4,16 +4,29 @@ import os
 
 app = Flask(__name__)
 
-# store tokens (temporary storage)
+ADMIN_CODE = "mySecret123"
 valid_tokens = {}
-
-# create a test token (expires in 60 seconds)
-valid_tokens["test123"] = time.time() + 60
 
 @app.route("/")
 def home():
     return "Server is running"
 
+
+# ADMIN: create token
+@app.route("/create-token")
+def create_token():
+    admin = request.args.get("admin")
+
+    if admin != ADMIN_CODE:
+        return abort(403)
+
+    token = "test123"
+    valid_tokens[token] = time.time() + 60
+
+    return token
+
+
+# DOWNLOAD
 @app.route("/download")
 def download():
     token = request.args.get("token")
@@ -21,15 +34,13 @@ def download():
     if token not in valid_tokens:
         return abort(403)
 
-    # check expiration
     if time.time() > valid_tokens[token]:
-        del valid_tokens[token]
         return abort(403)
 
-    # one-time use (delete after use)
     del valid_tokens[token]
 
     return send_file("pack.zip", as_attachment=True)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
