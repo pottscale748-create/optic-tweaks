4092253293775e8200d195baad2408ff27c53d68
pottscale748-create <pottscale748@gmail.com>
2026-03-21 17:52:16 -0700

    Update server.py

--- server.py
+++ server.py
@@ -12,16 +12,3 @@ def download():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-        return abort(403)
-
-    if time.time() > valid_tokens[token]:
-        return abort(403)
-
-    del valid_tokens[token]
-
-    return send_file("pack.zip", as_attachment=True)
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
