fcd02c3c99551f7b0fd8cd9806788ea4d500a0ef
pottscale748-create <pottscale748@gmail.com>
2026-03-21 17:43:17 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,37 +1,17 @@
-from flask import Flask, send_file, request, abort
-import time
-import os
+from flask import Flask, send_file
 
 app = Flask(__name__)
-
-ADMIN_CODE = "mySecret123"
-valid_tokens = {}
 
 @app.route("/")
 def home():
     return "Server is running"
 
-
-# ADMIN: create token
-@app.route("/create-token")
-def create_token():
-    admin = request.args.get("admin")
-
-    if admin != ADMIN_CODE:
-        return abort(403)
-
-    token = "test123"
-    valid_tokens[token] = time.time() + 60
-
-    return token
-
-
-# DOWNLOAD
 @app.route("/download")
 def download():
-    token = request.args.get("token")
+    return send_file("pack.zip", as_attachment=True)
 
-    if token not in valid_tokens:
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
         return abort(403)
 
     if time.time() > valid_tokens[token]:
