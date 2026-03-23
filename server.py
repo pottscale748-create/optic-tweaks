b2d29ec396866e90670080dbaac59c8340a0e9d1
pottscale748-create <pottscale748@gmail.com>
2026-03-22 23:47:23 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,12 +2,16 @@ from flask import Flask, send_file
 
 app = Flask(__name__)
 
+download_count = 0
+
 @app.route("/")
 def home():
-    return "Server is running"
+    return f"Downloads: {download_count}"
 
 @app.route("/download")
 def download():
+    global download_count
+    download_count += 1
     return send_file("pack.zip", as_attachment=True)
 
 if __name__ == "__main__":
